5d76b5db54e71843eb79c5ac38bd2c3dcd088999
poypoymignon <106139090+PoypoyMignon@users.noreply.github.com>
2023-02-27 23:05:10 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -10,11 +10,4 @@ const mobileMenu = () => {
 
 menu.addEventListener('click', mobileMenu)
 
-icon.onclick = function(){
-  document.body.classList.toggle("dark-theme");
-  if(document.body.classList.contains("dark-theme")){
-    icon.src = "images/moon.png"
-  }else {
-    icon.src = "images/sun.png";
-  }
-}
+  
